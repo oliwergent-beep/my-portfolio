bbca6533808920b6cdd3fb301d0242ac053812ff
Oliwer Gent <o.gent@aics.espritscholen.nl@Oliwers-MacBook-Air.local>
2026-03-29 12:51:11 +0200

    Added CSS styling and JS interactions

---
+++ js/script.js
@@ -0,0 +1,9 @@
+document.querySelectorAll('.btn').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        const targetId = this.getAttribute('href');
+        document.querySelector(targetId).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
